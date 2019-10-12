c7ecef2099bdc0ebc88e9a69f3a6ecc8b3d9534a
Jan Mathew <janmathew@Jans-MacBook-Pro.local>
2019-10-13 02:04:22 +0800

    removed console.log from randomTerm.js

--- src/config/randomTerm.js
+++ src/config/randomTerm.js
@@ -2,5 +2,5 @@ import sample from 'lodash/sample';
 
 const movieArray = ['batman', 'avengers', 'lego', 'love', 'one', 'black'];
 const randValue = sample(movieArray);
-console.log(randValue);
+
 export default randValue;
