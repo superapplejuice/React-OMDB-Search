b0f5421cd8769609c945d3044f13259f7f1ca830
Jan Mathew <janmathew@Jans-MacBook-Pro.local>
2019-10-13 11:59:07 +0800

    minor changes to handle loading in movie cards

--- src/components/movie/MovieCard.jsx
+++ src/components/movie/MovieCard.jsx
@@ -6,15 +6,37 @@ const MovieCard = ({ movie }) => {
 	const { Poster, Title, Year } = movie;
 
 	const renderMovieCard = () => {
+		const movieNotLoaded = () => {
+			return (
+				<div className="ui placeholder">
+					<div className="image">
+						<img src="" alt="movie has no poster!" />
+					</div>
+					<div className="content">
+						<div className="header"></div>
+						<div className="meta"></div>
+					</div>
+				</div>
+			);
+		};
+
+		const movieLoaded = () => {
+			return (
+				<>
+					<div className="image">
+						<img src={Poster} alt="movie has no poster!" />
+					</div>
+					<div className="content">
+						<div className="header">{Title}</div>
+						<div className="meta">{Year}</div>
+					</div>
+				</>
+			);
+		};
+
 		return (
 			<div className="ui link card" onClick={() => setShow(true)}>
-				<div className="image">
-					<img src={Poster} alt="movie has no poster!" />
-				</div>
-				<div className="content">
-					<div className="header">{Title}</div>
-					<div className="meta">{Year}</div>
-				</div>
+				{!Poster ? movieNotLoaded() : movieLoaded()}
 			</div>
 		);
 	};
